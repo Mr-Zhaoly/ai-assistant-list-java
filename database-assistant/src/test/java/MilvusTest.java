323b75cbca4ef2985a1edf88bef5e606af3f5663
larry <100705721@qq.com>
2025-12-02 23:09:54 +0800

    向量库配置调整

--- database-assistant/src/test/java/MilvusTest.java
+++ database-assistant/src/test/java/MilvusTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.ai.vectorstore.milvus.MilvusVectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
